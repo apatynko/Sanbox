13d000bf8a8f5e77fac5959cef9f53507117f310
apatynko <andriy.patinko@gmail.com>
2018-12-17 17:34:18 +0200

    renamed form

--- ListViewOwnerDrawSample/ListViewOwnerDrawSample/ListViewOwnerDraw.cs
+++ ListViewOwnerDrawSample/ListViewOwnerDrawSample/ListViewOwnerDraw.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ListViewOwnerDrawSample
 {
-    public partial class Form1 : Form
+    public partial class ListViewOwnerDraw : Form
     {
-        public Form1()
+        public ListViewOwnerDraw()
         {
             InitializeComponent();
         }
