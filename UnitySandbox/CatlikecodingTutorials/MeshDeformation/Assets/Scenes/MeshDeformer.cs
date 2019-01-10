cc7de129612bc5494fdc561e0cc7613e0514c72a
apatynko <andriy.patinko@gmail.com>
2019-01-10 10:31:30 +0200

    Mesh Deformation Making a Stress Ball

---
+++ UnitySandbox/CatlikecodingTutorials/MeshDeformation/Assets/Scenes/MeshDeformer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour {
+
+	Mesh deformingMesh;
+	Vector3[] originalVertices, displacedVertices;
+	Vector3[] vertexVelocities;
+
+	void Start () {
+		deformingMesh = GetComponent<MeshFilter>().mesh;
+		originalVertices = deformingMesh.vertices;
+		displacedVertices = new Vector3[originalVertices.Length];
+		for (int i = 0; i < originalVertices.Length; i++) {
+			displacedVertices[i] = originalVertices[i];
+		}
+		vertexVelocities = new Vector3[originalVertices.Length];
+	}
+
+	public void AddDeformingForce (Vector3 point, float force) {
+		Debug.DrawLine(Camera.main.transform.position, point);
+	}
+}
