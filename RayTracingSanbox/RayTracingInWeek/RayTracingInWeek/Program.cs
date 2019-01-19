afc6984015c9d054b7e79d55a0c42215806d56c9
apatynko <andriy.patinko@gmail.com>
2019-01-19 11:08:23 +0200

    created a solution for raytraing in a weekend

---
+++ RayTracingSanbox/RayTracingInWeek/RayTracingInWeek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracingInWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
