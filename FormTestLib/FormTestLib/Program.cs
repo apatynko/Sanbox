8a7cefec6b363bf86d625820554001225737f348
apatynko <andriy.patinko@gmail.com>
2019-03-12 17:43:03 +0200

    stackoverflow answer for high DPI support

---
+++ FormTestLib/FormTestLib/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormTestLib
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            ValidatingSplash Splash = new ValidatingSplash();
+            Splash.ShowDialog();
+        }
+    }
+}
