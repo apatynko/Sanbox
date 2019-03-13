1940dc02ff87a7824beafd5d22367e24e6e49cf5
apatynko <andriy.patinko@gmail.com>
2019-03-13 16:24:40 +0200

    second question about dpi-aware
    https://stackoverflow.com/questions/55108044/how-to-get-rid-of-this-edge-in-net-winforms-app/55144148#55144148

--- FormTestLib/FormTestLib/ValidatingSplash.Designer.cs
+++ FormTestLib/FormTestLib/ValidatingSplash.Designer.cs
@@ -28,41 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ValidatingSplash));
             this.lblValidating = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
-
             // 
             // lblValidating
             // 
             this.lblValidating.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblValidating.AutoSize = true;
-            this.lblValidating.Location = new System.Drawing.Point(58, 45);
+            this.lblValidating.Location = new System.Drawing.Point(191, 129);
             this.lblValidating.Name = "lblValidating";
             this.lblValidating.Size = new System.Drawing.Size(166, 13);
             this.lblValidating.TabIndex = 7;
             this.lblValidating.Text = "Validating cached credentials...";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 160);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(302, 47);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 213);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(302, 47);
+            this.panel2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Security";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Network Settings";
+            // 
             // ValidatingSplash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 104);
+            this.ClientSize = new System.Drawing.Size(541, 700);
             this.ControlBox = false;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblValidating);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            //this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ValidatingSplash";
             this.Text = "Validating Credentials";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
         private System.Windows.Forms.Label lblValidating;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
