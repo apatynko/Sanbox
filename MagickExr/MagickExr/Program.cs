9582afa030fd90e3ecce0ca419962ce36e6bda6c
apatynko <andriy.patinko@gmail.com>
2019-09-05 13:15:41 +0300

    magickexr sample
    magickexr

---
+++ MagickExr/MagickExr/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ImageMagick;
+
+namespace MagickExr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MagickImage hdrImage =
+                new MagickImage(
+                    @"d:\DEV_stuff\DEV\sharpexr\EXRViewer\bin\Debug\AmericanKitchen_OI2_2.VRayDiffuseFilterPS.exr ", new PixelReadSettings()))
+            {
+                Console.WriteLine("Reading image");
+                IPixelCollection pixels = hdrImage.GetPixels();
+                for (int y = 0; y < hdrImage.Height; y++)
+                {
+                    for (int x = 0; x < hdrImage.Width; x++)
+                    {
+                        MagickColor p = pixels.GetPixel(x, y).ToColor();
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
