7fbb8a54b85638ad17e607c04a0e16e288edfef2
apatynko <andriy.patinko@gmail.com>
2021-07-23 20:37:41 +0300

    Initial commeit of the app

---
+++ SOLIDApp/SOLIDApp/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLIDApp
+{
+    internal class Person
+    {
+        public Person()
+        {
+        }
+
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+    }
+}
---
+++ SOLIDApp/SOLIDApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SOLIDApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my application!");
+
+            //Ask for user information
+
+            Person user = new Person();
+
+            Console.WriteLine("What is your first name: ");
+            user.FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name: ");
+            user.LastName = Console.ReadLine();
+
+            //Checks to be sure the first and last names are valid
+            if (string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                Console.WriteLine("You did not give us a valid first name!");
+                Console.ReadLine();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine("You did not give us a valid last name!");
+                Console.ReadLine();
+                return;
+            }
+
+            // Create a username for the person
+            Console.WriteLine($"Your username is {user.FirstName.Substring(0, 1)}{user.LastName}");
+            Console.ReadLine();
+        }
+    }
+}
